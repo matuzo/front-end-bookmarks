4ada71375e066ef2b56f3df7ef3ec84ffb078c4c
Manuel Matuzovic <m.matuzovic@nousdigital.net>
2020-03-20 11:31:42 +0100

    Use custom metascraper for author

---
+++ src/_11ty/metascraper-author.js
@@ -0,0 +1,31 @@
+'use strict'
+
+const { $jsonld, $filter, toRule, author } = require('@metascraper/helpers')
+
+const REGEX_STRICT = /^\S+\s+\S+/
+
+const toAuthor = toRule(author)
+
+/**
+ * Enforce stricter matching for a `rule`.
+ *
+ * @param {Function} rule
+ * @return {Function} stricter
+ */
+
+const strict = rule => $ => {
+  const value = rule($)
+  return REGEX_STRICT.test(value) && value
+}
+
+module.exports = () => ({
+  author: [
+    toAuthor($jsonld('author.name')),
+    toAuthor($ => $('meta[name="author"]').attr('content')),
+    toAuthor($ => $('meta[property="author"]').attr('content')),
+    toAuthor($ => $('meta[property="article:author"]').attr('content')),
+    toAuthor($ => $filter($, $('[itemprop*="author" i] [itemprop="name"]'))),
+    toAuthor($ => $filter($, $('[itemprop*="author" i]'))),
+    toAuthor($ => $filter($, $('[rel="author"]')))
+  ]
+})
